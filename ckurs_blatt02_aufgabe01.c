fded220ef028d80dc42a4f5f26758a499e91e74a
Mohamed Assaleh <assaleh@campus.tu-berlin.de>
2020-11-06 21:32:25 +0100

    upload ckurs_blatt02_aufgabe01.c_3

--- ckurs_blatt02_aufgabe01.c
+++ ckurs_blatt02_aufgabe01.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main() {
     int nummer = 105; // Die probierten Werte
@@ -10,11 +9,12 @@ int main() {
         return 0;
     }
 
-    for (int i = 2; i <= sqrt(nummer); i++)
+    for (int i = 2; i <= nummer - 1; i++)
         if (nummer % i == 0) {
             printf("Nein\n");
             return 0;
         }
+
     printf("Ja\n");
     return 0;
 
