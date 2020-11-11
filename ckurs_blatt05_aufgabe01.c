801579e597d14eaa7f460c706c52c03a307a44ca
Mohamed Fathi H Assaleh <assaleh@campus.tu-berlin.de>
2020-11-11 21:51:48 +0100

    Update ckurs_blatt05_aufgabe01.c_5

--- ckurs_blatt05_aufgabe01.c
+++ ckurs_blatt05_aufgabe01.c
@@ -1,56 +1,56 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 void print_array(int array[], int len){
-  printf("Array: %d", array[0]);
-  for(int i = 1; i < len; i++){
-    printf(", %d",array[i]);
-  }
-  printf("\n");
+    printf("Array: %d", array[0]);
+    for(int i = 1; i < len; i++){
+        printf(", %d",array[i]);
+    }
+    printf("\n");
 }
 
-int sum(int array[], int len){
-  int sum = 0;
-  for(int i = 0; i < len; i++){
-    sum += array[i];
-  }
-  return sum;
+int sum(int array[], int len, int *p){
+    int sum = 0;
+    for(int i = 0; i < len; i++){
+        sum += array[i];
+        *p = sum;
+    }
 }
 
 int  max(int array[], int len){
-  int max = array[0];
-  for(int i = 1; i < len; i++){
-    if(array[i] > max){
-      max = array[i];
+    int max = array[0];
+    for(int i = 1; i < len; i++){
+        if(array[i] > max){
+            max = array[i];
+        }
     }
-  }
-  return max;
+    return max;
 }
 
 int  min(int array[], int len){
-  int min = array[0];
-  for(int i = 1; i < len; i++){
-    if(array[i] < min){
-      min = array[i];
+    int min = array[0];
+    for(int i = 1; i < len; i++){
+        if(array[i] < min){
+            min = array[i];
+        }
     }
-  }
-  return min;
+    return min;
 }
 
 int main(){
-  int array[]={9,4,7,8,10,5,1,6,3,2};
-int len = 10;
+    int array[] ={2,4,5,6,2,5,2,5,2,3};
+    int len = 10;
 
-  print_array(array, len);
+    print_array(array, len);
 
-  int min_num = min(array, len);
-  printf("Minimum: %d\n", min_num);
+    int min_num = min(array, len);
+    printf("Minimum: %d\n", min_num);
 
-  int max_num = max(array, len);
-  printf("Maximum: %d\n", max_num);
+    int max_num = max(array, len);
+    printf("Maximum: %d\n", max_num);
 
-  int sum_num = sum(array, len);
-  printf("Summe: %d\n",sum_num);
-
-  return 0;
+    int p;
+    sum(array, len, &p);
+    printf("Summe: %d", p);
+    
+    return 0;
 }
