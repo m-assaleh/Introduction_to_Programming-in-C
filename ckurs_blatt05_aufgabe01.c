3092f43fd8d4a26d7d655ff7c63be16fca3ffb57
Mohamed Fathi H Assaleh <assaleh@campus.tu-berlin.de>
2020-11-11 21:44:04 +0100

    Update ckurs_blatt05_aufgabe01.c_3

--- ckurs_blatt05_aufgabe01.c
+++ ckurs_blatt05_aufgabe01.c
@@ -9,42 +9,72 @@ void print_array(int array[], int len) {
     printf("\n");
 }
 
-void print_sum(int array[], int len){
-    int sum = 0;
-    for(int i = 0; i < len; i++){
+
+int sum(int array[], int len, int *p) {
+    int sum;
+    for (int i = 0; i < len-1 ; ++i) {
         sum = sum + array[i];
+        *p = sum;
     }
-    printf("Summe: %d\n",sum);
 }
 
-void print_min(int array[], int len){
-    int min = array[0];
-    for(int i = 1; i < len; i++){
-        if(array[i] < min){
-            min = array[i];
-        }
-    }
-    printf("Minimum: %d\n", min);
+int min(int array[], int len) {
+
+int min = array[0];
+for (int i = 1; i < len; ++i) {
+if (array[i] < min) {
+min = array[i];
+}
 }
 
-void print_max(int array[], int len){
+return min;
+
+}
+
+int max(int array[], int len) {
+
     int max = array[0];
-    for(int i = 1; i < len; i++){
-        if(array[i] > max){
+    for (int i = 1; i < len; ++i) {
+        if (array[i] > max) {
             max = array[i];
         }
     }
-    printf("Maximum: %d\n", max);
+
+    return max;
+
 }
+
+
 
 int main() {
     int array[] = {9, 4, 7, 8, 10, 5, 1, 6, 3, 2};
     int len = 10;
-
     print_array(array, len);
-    print_min(array, len);
-    print_max(array, len);
-    print_sum(array, len);
+   
 
-    return 0;
+    int min = min(array, len);
+    printf("Minimum: %d\n", min);
+
+    int max = max(array, len);
+    printf("Maximum: %d\n",max);
+    int p;
+    sum(array, len, &p);
+    printf("Summe: %d", p);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
