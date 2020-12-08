cb380959e299f4241d5882989120b2ec3055b399
Mohamed Fathi H Assaleh <assaleh@campus.tu-berlin.de>
2020-12-08 03:49:01 +0100

    Update introprog_countsort.c

--- introprog_countsort.c
+++ introprog_countsort.c
@@ -11,18 +11,17 @@ void count_sort_calculate_counts(int input_array[], int len, int count_array[]) 
         count_array[i] = 0;
     }
 
-    for (int j = 0; j < len; ++j) {
-        count_array[input_array[j]] = count_array[input_array[j]]+1;
+    for (int i = 0; i < len; ++i) {
+        count_array[input_array[i]] = 1 + count_array[input_array[i]];
     }
 }
 
 void count_sort_write_output_array(int output_array[], int len, int count_array[]) {
     /* Hier Funktion implementieren */
-    int k = 0;
-    for (int i = 0; i <= MAX_LAENGE; ++i) {
+    for (int i, x = 0; i <= MAX_LAENGE; ++i) {
         for (int j = 0; j < count_array[i]; ++j) {
-            output_array[k] = i;
-            k = k + 1;
+            output_array[x] = i;
+            x = 1 + x;
         }
     }
 }
