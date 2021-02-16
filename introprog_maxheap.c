474cc1c647c1a10af744cb1773eb96833dd90b55
Mohamed Fathi H Assaleh <assaleh@campus.tu-berlin.de>
2021-02-16 03:43:37 +0100

    Update introprog_maxheap.c

--- introprog_maxheap.c
+++ introprog_maxheap.c
@@ -25,7 +25,7 @@ heap* heap_create(size_t capacity) {
     hp -> elements = (int*)malloc(capacity * sizeof (int));
     hp -> capacity = capacity;
     hp -> size = 0;
-    
+
     return hp;
 
 }
@@ -38,38 +38,38 @@ heap* heap_create(size_t capacity) {
  */
 
 void heapify(heap* h, size_t i) {
-    
+
     size_t l = (i + 1) * 2 - 1;
     size_t r = (i + 1) * 2;
 
     size_t largest;
 
     if (l < h -> size && h -> elements[l] > h -> elements[i]){
-        
+
         largest = l;
-        
+
     } else {
-        
+
         largest = i;
-        
+
     }
-    
+
     if (r < h -> size && h -> elements[r] > h -> elements[largest]){
 
         largest=r;
 
     }
-    
+
     if (largest != i ){
-        
+
         size_t m =   h -> elements[i];
         h -> elements[i] = h -> elements[largest];
         h -> elements[largest] = m;
-        
+
         heapify(h, largest);
 
     }
-    
+
 }
 
 
@@ -82,18 +82,18 @@ void heapify(heap* h, size_t i) {
 int heap_extract_max(heap* h) {
 
     int max;
-    
+
     if(h -> size < 1){
-        
+
         return -1;
-        
+
     }
 
     max = h -> elements[0];
     h -> elements[0] = h -> elements[h -> size - 1];
     h -> size = h -> size - 1;
     heapify(h, 0);
-    
+
     return max;
 
 }
@@ -114,9 +114,9 @@ int heap_insert(heap* h, int key) {
     }
     h -> size = (h -> size) + 1;
 
-    while(i != 0 && h -> elements[(i + 1) / 2] < key)
+    while(i != 0 && h -> elements[(i + 1) / 2 - 1] < key)
     {
-        h -> elements[i] = h -> elements[(i + 1) / 2 - 2];
+        h -> elements[i] = h -> elements[(i + 1) / 2 - 1];
         i = (i + 1)/ 2 - 1;
     }
     h -> elements[i] = key;
@@ -130,10 +130,10 @@ int heap_insert(heap* h, int key) {
  */
 
 void heap_free(heap* h) {
-    
+
     free(h -> elements);
-    
+
     free(h);
-    
+
 }
 
