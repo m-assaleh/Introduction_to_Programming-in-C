4c63b27f4062da804954c20971e7e5f3aba6ba4a
Mohamed Fathi H Assaleh <assaleh@campus.tu-berlin.de>
2021-02-16 03:36:13 +0100

    Update introprog_maxheap.c

--- introprog_maxheap.c
+++ introprog_maxheap.c
@@ -5,6 +5,19 @@
 
 #include "introprog_heap.h"
 
+/* Reserviere Speicher für einen Heap
+ *
+ * Der Heap soll dabei Platz für capacity Elemente bieten.
+ *
+ * Um konsistent mit den Parametern für malloc zu sein, ist
+ * capacity als size_t (entspricht unsigned long auf x86/64)
+ * deklariert.
+ * Da es sauberer ist, sind auch alle Indizes in dieser Aufgabe
+ * als size_t deklariert. Ob man size_t oder int als Index für
+ * ein Array verwendet, macht in der Praxis (auf x86/64) nur
+ * bei Arrays mit mehr als 2.147.483.647 Elementen einen
+ * Unterschied.
+ */
 heap* heap_create(size_t capacity) {
 
     heap * hp = (heap*)malloc(sizeof(heap));
@@ -12,11 +25,19 @@ heap* heap_create(size_t capacity) {
     hp -> capacity = capacity;
     hp -> size = 0;
     return hp;
+
 }
 
+/* Stellt die Heap Bedingung an Index i wieder her
+ *
+ * Vorraussetzung: Der linke und rechte Unterbaum von i
+ * erfüllen die Heap-Bedingung bereits.
+ */
 void heapify(heap* h, size_t i) {
-    size_t l = (i+1)*2 - 1;
-    size_t r = (i+1) * 2;
+
+
+    size_t l = (i)*2;
+    size_t r = (i * 2)+1;
 
     size_t largest;
 
@@ -37,8 +58,15 @@ void heapify(heap* h, size_t i) {
         heapify(h, largest);
 
     }
+    
+
 }
 
+/* Holt einen Wert aus dem Heap
+ *
+ * Wenn der Heap nicht leer ist, wird die größte Zahl
+ * zurückgegeben. Wenn der Heap leer ist, wird -1 zurückgegeben.
+ */
 int heap_extract_max(heap* h) {
 
     int max;
@@ -47,13 +75,18 @@ int heap_extract_max(heap* h) {
         return -1;
     }
 
-        max = h -> elements[0];
-        h -> elements[0] = h -> elements[h -> size-1];
-        h -> size = h -> size - 1;
-        heapify(h, 0);
-        return max;
+    max = h -> elements[1];
+    h -> elements[1] = h -> elements[h -> size];
+    h -> size = h -> size - 1;
+    heapify(h, 1);
+    return max;
+    
 }
 
+/* Fügt einen Wert in den Heap ein
+ *
+ * Wenn der Heap bereits voll ist, wird -1 zurückgegeben.
+ */
 int heap_insert(heap* h, int key) {
 
     size_t i = h -> size;
@@ -64,15 +97,19 @@ int heap_insert(heap* h, int key) {
     }
     h -> size = (h -> size) +1;
 
-    while(i != 0 && h -> elements[(i+1)/2 -1] < key)
+    while(i != 0 && h -> elements[(i)/2] < key)
     {
-        h -> elements[i] = h -> elements[(i+1)/2 -1];
-        i = (i+1)/2 - 1;
+        h -> elements[i] = h -> elements[(i)/2];
+        i = (i)/2;
     }
     h -> elements[i] = key;
     return 0;
+    
+    
 }
 
+/* Gibt den Speicher von einem Heap wieder frei
+ */
 void heap_free(heap* h) {
     free(h -> elements);
     free(h);
